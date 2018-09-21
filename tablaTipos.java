a0bf8d07f358db8ef529fefd570f21a0e17b17e7
Hector <hector.gomez1997@gmail.com>
2018-09-20 19:19:27 -0500

    Import nuevo
    Agrege un import que no conozco y no se que es xD

--- tablaTipos.java
+++ tablaTipos.java
@@ -3,6 +3,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import java.applet.*;
+
 public class tablaTipos {
 
     public static final String FILENAME = "texto.txt";
